5bb24342ffbde8d4f655f40adb46e471d3e63e94
= <kidthephantomthieft1412@gmail.com>
2023-03-16 18:26:43 +0700

    main

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: const LandingPage(),
-      home: const HomePage(),
-      theme: ThemeData(primarySwatch: Colors.blue,),
+      home: const LandingPage(),
+      // home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       builder: EasyLoading.init(),
     );
   }
