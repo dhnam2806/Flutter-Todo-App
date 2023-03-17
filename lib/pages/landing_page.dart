ea2998d2ea6bbd965ab8256689eb701004f5e4cd
= <kidthephantomthieft1412@gmail.com>
2023-03-17 16:05:03 +0700

    font update

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -23,7 +23,8 @@ class LandingPage extends StatelessWidget {
                       child: const Text('TO DO APP',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 50.0,
+                            fontSize: 60.0,
+                            fontFamily: 'Rubik'
                           )),
                     ),
                     Container(
@@ -32,7 +33,8 @@ class LandingPage extends StatelessWidget {
                           'Made by: Team 8',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 20,
+                            fontSize: 28,
+                            fontFamily: 'Rubik'
                           ),
                         )),
                   ]),
